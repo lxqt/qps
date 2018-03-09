af29ebd562fb2afb8a1ac1ef0d923720c72f9a76
scootergrisen <scootergrisen@gmail.com>
2018-03-09 23:34:13 +0100

    Create qps_da.ts

---
+++ src/translations/qps_da.ts
@@ -0,0 +1,171 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="da">
+<context>
+    <name>ControlBar</name>
+    <message>
+        <location filename="../misc.cpp" line="964"/>
+        <source>Pause (Ctrl+Space)</source>
+        <translation>Pause (Ctrl+Mellemrum)</translation>
+    </message>
+</context>
+<context>
+    <name>EventDialog</name>
+    <message>
+        <location filename="../watchdog.ui" line="23"/>
+        <source>Watchdog 0.1 alpha</source>
+        <translation>Watchdog 0.1 alfa</translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="117"/>
+        <source>Eventcat</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="162"/>
+        <source>Select condition</source>
+        <translation>Vælg betingelse</translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="170"/>
+        <source>labelDescrition</source>
+        <translation>etiketbeskrivelse</translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="193"/>
+        <source>Enable</source>
+        <translation>Aktivér</translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="219"/>
+        <source>process name</source>
+        <translation>procesnavn</translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="229"/>
+        <source>cpu</source>
+        <translation>cpu</translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="248"/>
+        <source>%</source>
+        <translation> %</translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="280"/>
+        <source>include already running process</source>
+        <translation>inkluder allerede kørende proces</translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="309"/>
+        <source>run command</source>
+        <translation>kør kommando</translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="342"/>
+        <source>show Message</source>
+        <translation>vis meddelelse</translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="367"/>
+        <source>Help (Not yet. just concept)</source>
+        <translation>Hjælp (ikke endnu, kun et koncept)</translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="394"/>
+        <source>%p : pid
+%c : command</source>
+        <translation>%p: pid
+%c: kommando</translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="425"/>
+        <source>New</source>
+        <translation>Ny</translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="432"/>
+        <source>Add</source>
+        <translation>Tilføj</translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="439"/>
+        <source>Delete</source>
+        <translation>Slet</translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="446"/>
+        <source>Close</source>
+        <translation>Luk</translation>
+    </message>
+</context>
+<context>
+    <name>ExecWindow</name>
+    <message>
+        <location filename="../message.ui" line="13"/>
+        <source>Qps</source>
+        <translation>Qps</translation>
+    </message>
+    <message>
+        <location filename="../message.ui" line="27"/>
+        <source>Ok</source>
+        <translation>OK</translation>
+    </message>
+</context>
+<context>
+    <name>Qps</name>
+    <message>
+        <location filename="../qps.cpp" line="335"/>
+        <source>Detail</source>
+        <translation>Detalje</translation>
+    </message>
+</context>
+<context>
+    <name>Screenshot</name>
+    <message>
+        <location filename="../screenshot.cpp" line="271"/>
+        <source>/untitled.</source>
+        <translation>/unavngivet.</translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="276"/>
+        <source>Save As</source>
+        <translation>Gem som</translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="277"/>
+        <source>%1 Files (*.%2);;All Files (*)</source>
+        <translation>%1 filer (*.%2);;Alle filer (*)</translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="304"/>
+        <source>Options</source>
+        <translation>Valgmuligheder</translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="307"/>
+        <source> s</source>
+        <translation> s</translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="312"/>
+        <source>Screenshot Delay:</source>
+        <translation>Forsinkelse for skærmbillede:</translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="314"/>
+        <source>Hide This Window</source>
+        <translation>Skjul vinduet</translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="326"/>
+        <source>New Screenshot</source>
+        <translation>Nyt skærmbillede</translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="327"/>
+        <source>Quit</source>
+        <translation>Afslut</translation>
+    </message>
+</context>
+</TS>
