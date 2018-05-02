0466ff5a471078c38e48239826fd5f8c4418ec61
Alf <agaida@siduction.org>
2018-05-02 22:35:34 +0000

    Added translation using Weblate (German)

---
+++ src/translations/qps_de.ts
@@ -0,0 +1,170 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de">
+<context>
+    <name>ControlBar</name>
+    <message>
+        <location filename="../misc.cpp" line="986"/>
+        <source>Pause (Ctrl+Space)</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>EventDialog</name>
+    <message>
+        <location filename="../watchdog.ui" line="23"/>
+        <source>Watchdog 0.1 alpha</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="117"/>
+        <source>Eventcat</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="162"/>
+        <source>Select condition</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="170"/>
+        <source>labelDescrition</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="193"/>
+        <source>Enable</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="219"/>
+        <source>process name</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="229"/>
+        <source>cpu</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="248"/>
+        <source>%</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="280"/>
+        <source>include already running process</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="309"/>
+        <source>run command</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="342"/>
+        <source>show Message</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="367"/>
+        <source>Help (Not yet. just concept)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="394"/>
+        <source>%p : pid
+%c : command</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="425"/>
+        <source>New</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="432"/>
+        <source>Add</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="439"/>
+        <source>Delete</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../watchdog.ui" line="446"/>
+        <source>Close</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>ExecWindow</name>
+    <message>
+        <location filename="../message.ui" line="13"/>
+        <source>Qps</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../message.ui" line="27"/>
+        <source>Ok</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Qps</name>
+    <message>
+        <location filename="../qps.cpp" line="351"/>
+        <source>Detail</source>
+        <translation></translation>
+    </message>
+</context>
+<context>
+    <name>Screenshot</name>
+    <message>
+        <location filename="../screenshot.cpp" line="294"/>
+        <source>/untitled.</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="299"/>
+        <source>Save As</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="300"/>
+        <source>%1 Files (*.%2);;All Files (*)</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="327"/>
+        <source>Options</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="330"/>
+        <source> s</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="335"/>
+        <source>Screenshot Delay:</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="337"/>
+        <source>Hide This Window</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="349"/>
+        <source>New Screenshot</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../screenshot.cpp" line="350"/>
+        <source>Quit</source>
+        <translation></translation>
+    </message>
+</context>
+</TS>
