477a80818545c36df0f1adab61a645010ce95a0b
Yaron Shahrabani <sh.yaron@gmail.com>
2018-06-13 14:56:13 +0000

    Translated using Weblate (Hebrew)
    Currently translated at 100.0% (30 of 30 strings)
    
    Translation: LXQt/QPS
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/qps/he/

--- src/translations/qps_he.ts
+++ src/translations/qps_he.ts
@@ -6,7 +6,7 @@
     <message>
         <location filename="../misc.cpp" line="986"/>
         <source>Pause (Ctrl+Space)</source>
-        <translation></translation>
+        <translation>עצירה (Ctrl+Space)</translation>
     </message>
 </context>
 <context>
@@ -14,7 +14,7 @@
     <message>
         <location filename="../watchdog.ui" line="23"/>
         <source>Watchdog 0.1 alpha</source>
-        <translation></translation>
+        <translation>משגיח 0.1 אלפא</translation>
     </message>
     <message>
         <location filename="../watchdog.ui" line="117"/>
@@ -24,7 +24,7 @@
     <message>
         <location filename="../watchdog.ui" line="162"/>
         <source>Select condition</source>
-        <translation></translation>
+        <translation>בחירת מצב</translation>
     </message>
     <message>
         <location filename="../watchdog.ui" line="170"/>
@@ -34,17 +34,17 @@
     <message>
         <location filename="../watchdog.ui" line="193"/>
         <source>Enable</source>
-        <translation></translation>
+        <translation>הפעלה</translation>
     </message>
     <message>
         <location filename="../watchdog.ui" line="219"/>
         <source>process name</source>
-        <translation></translation>
+        <translation>שם תהליך</translation>
     </message>
     <message>
         <location filename="../watchdog.ui" line="229"/>
         <source>cpu</source>
-        <translation></translation>
+        <translation>מעבד</translation>
     </message>
     <message>
         <location filename="../watchdog.ui" line="248"/>
@@ -54,22 +54,22 @@
     <message>
         <location filename="../watchdog.ui" line="280"/>
         <source>include already running process</source>
-        <translation></translation>
+        <translation>כולל תהליך שכבר רץ</translation>
     </message>
     <message>
         <location filename="../watchdog.ui" line="309"/>
         <source>run command</source>
-        <translation></translation>
+        <translation>הפעלת פקודה</translation>
     </message>
     <message>
         <location filename="../watchdog.ui" line="342"/>
         <source>show Message</source>
-        <translation></translation>
+        <translation>הצגת הודעה</translation>
     </message>
     <message>
         <location filename="../watchdog.ui" line="367"/>
         <source>Help (Not yet. just concept)</source>
-        <translation></translation>
+        <translation>עזרה (בינתיים אין, זה בגדר רעיון)</translation>
     </message>
     <message>
         <location filename="../watchdog.ui" line="394"/>
@@ -80,22 +80,22 @@
     <message>
         <location filename="../watchdog.ui" line="425"/>
         <source>New</source>
-        <translation></translation>
+        <translation>חדש</translation>
     </message>
     <message>
         <location filename="../watchdog.ui" line="432"/>
         <source>Add</source>
-        <translation></translation>
+        <translation>הוספה</translation>
     </message>
     <message>
         <location filename="../watchdog.ui" line="439"/>
         <source>Delete</source>
-        <translation></translation>
+        <translation>מחיקה</translation>
     </message>
     <message>
         <location filename="../watchdog.ui" line="446"/>
         <source>Close</source>
-        <translation></translation>
+        <translation>סגירה</translation>
     </message>
 </context>
 <context>
@@ -108,7 +108,7 @@
     <message>
         <location filename="../message.ui" line="27"/>
         <source>Ok</source>
-        <translation></translation>
+        <translation>אישור</translation>
     </message>
 </context>
 <context>
@@ -116,7 +116,7 @@
     <message>
         <location filename="../qps.cpp" line="351"/>
         <source>Detail</source>
-        <translation></translation>
+        <translation>פרטים</translation>
     </message>
 </context>
 <context>
@@ -129,17 +129,17 @@
     <message>
         <location filename="../screenshot.cpp" line="299"/>
         <source>Save As</source>
-        <translation></translation>
+        <translation>שמירה בשם</translation>
     </message>
     <message>
         <location filename="../screenshot.cpp" line="300"/>
         <source>%1 Files (*.%2);;All Files (*)</source>
-        <translation></translation>
+        <translation>קובצי %1 (‎*.%2);;כל הקבצים (*)</translation>
     </message>
     <message>
         <location filename="../screenshot.cpp" line="327"/>
         <source>Options</source>
-        <translation></translation>
+        <translation>אפשרויות</translation>
     </message>
     <message>
         <location filename="../screenshot.cpp" line="330"/>
@@ -149,22 +149,22 @@
     <message>
         <location filename="../screenshot.cpp" line="335"/>
         <source>Screenshot Delay:</source>
-        <translation></translation>
+        <translation>השהיית צילום מסך:</translation>
     </message>
     <message>
         <location filename="../screenshot.cpp" line="337"/>
         <source>Hide This Window</source>
-        <translation></translation>
+        <translation>הסתרת החלון הזה</translation>
     </message>
     <message>
         <location filename="../screenshot.cpp" line="349"/>
         <source>New Screenshot</source>
-        <translation></translation>
+        <translation>צילום מסך חדש</translation>
     </message>
     <message>
         <location filename="../screenshot.cpp" line="350"/>
         <source>Quit</source>
-        <translation></translation>
+        <translation>יציאה</translation>
     </message>
 </context>
 </TS>
