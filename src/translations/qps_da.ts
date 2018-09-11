42d3a3de28d37dc9a57f31c061870a3ed5d32c3e
scootergrisen <scootergrisen@gmail.com>
2018-09-11 19:57:23 +0000

    Translated using Weblate (Danish)
    Currently translated at 51,2% (169 of 330 strings)
    
    Translation: LXQt/QPS
    Translate-URL: https://weblate.lxqt.org/projects/lxqt/qps/da/

--- src/translations/qps_da.ts
+++ src/translations/qps_da.ts
@@ -1658,7 +1658,7 @@ afsluttede med status </translation>
     <message>
         <location filename="../details.cpp" line="151"/>
         <source>File descriptor</source>
-        <translation type="unfinished">Filbeskriver</translation>
+        <translation>Filbeskriver</translation>
     </message>
     <message>
         <location filename="../details.cpp" line="152"/>
