5bc8c9812abf180b854329d2bc9e8119a180e491
Luís Pereira <luis.artur.pereira@gmail.com>
2020-02-14 18:03:10 +0000

    Reorganize Svec class
    Stop including svec.cpp.
    Just move the needed stuff to svec.h.
    Makes much easier to maintain and amenable to automated tools.

--- src/svec.h
+++ src/svec.h
@@ -27,6 +27,10 @@
 #include <iostream>
 #include <stdlib.h>
 
+extern "C" {
+typedef int (*compare_func)(const void *, const void *);
+}
+
 template <typename Key, typename T> class SHash : public std::map<Key, T>
 {
   public:
@@ -97,6 +101,13 @@ template <class T> inline Svec<T>::~Svec() { free(vect); }
 
 template <class T> inline int Svec<T>::size() const { return used; }
 
+template <class T> void Svec<T>::setSize(int newsize)
+{
+    while (newsize > alloced)
+        grow();
+    used = newsize;
+}
+
 template <class T> inline T &Svec<T>::operator[](int i)
 {
 #ifdef CHECK_INDICES
@@ -149,6 +160,122 @@ template <class T> inline void Svec<T>::grow()
 {
     //	printf("size=%d\n",sizeof(T));
     vect = (T *)realloc(vect, (alloced *= 2) * sizeof(T));
+}
+
+template <class T> void Svec<T>::setextend(int index, T value)
+{
+#ifdef CHECK_INDICES
+    if (index < 0)
+        fatal("const Svec: negative index");
+#endif
+    while (index >= alloced)
+        grow();
+    if (index >= used)
+        used = index + 1;
+    vect[index] = value;
+}
+
+template <class T> Svec<T>::Svec(const Svec<T> &s)
+{
+    int n = s.size();
+    if (n < 8)
+        n = 8;
+    vect = (T *)malloc(n * sizeof(T));
+    alloced = n;
+    used = s.size();
+    for (int i = 0; i < used; i++)
+        vect[i] = s[i];
+}
+
+template <class T> Svec<T> &Svec<T>::operator=(const Svec<T> &s)
+{
+    if (this != &s)
+    {
+        if (alloced < s.size())
+        {
+            alloced = s.size();
+            vect = (T *)realloc(vect, alloced * sizeof(T));
+        }
+        for (int i = 0; i < s.size(); i++)
+        {
+            vect[i] = s.vect[i];
+        }
+        used = s.size();
+    }
+    return *this;
+}
+
+/*
+template<class T>
+void Svec<T>::indexerr(int index) const
+{
+    fatal("Svec: index out of range (%d, valid is 0..%d)", index, used - 1);
+}
+*/
+
+template <class T> void Svec<T>::insert(int index, T value)
+{
+#ifdef CHECK_INDICES
+    if (index < 0 || index > used)
+        fatal("Svec: index out of range");
+#endif
+    if ((used + 1) > alloced)
+        grow();
+
+    /*int i;
+    T v;
+          v=vect[i+1];
+    for(i = index; j < =used ; i++)
+        vect[i+1]=v;
+        vect[i+1]=vect[i]; //vect[index+1]=vect[index];
+        */
+
+    // old
+    for (int j = used - 1; j >= index; j--)
+        vect[j + 1] = vect[j];
+
+    vect[index] = value;
+
+    used++;
+}
+
+// for int,float type , no delete
+template <class T> void Svec<T>::remove(int index)
+{
+#ifdef CHECK_INDICES
+    if (index < 0 || index >= used)
+        fatal("Svec: index out of range");
+#endif
+    for (int j = index; j < used - 1; j++)
+        vect[j] = vect[j + 1];
+    used--;
+}
+
+// for class type
+template <class T> void Svec<T>::Delete(int index)
+{
+#ifdef CHECK_INDICES
+    if (index < 0 || index >= used)
+        fatal("Svec: index out of range");
+#endif
+    delete vect[index];
+    for (int j = index; j < used - 1; j++)
+        vect[j] = vect[j + 1];
+    used--;
+}
+
+// Assuming T is "pointer to U", delete all contents.
+// Warning: duplicated objects will be deleted twice --- doubleplusungood
+template <class T> void Svec<T>::purge()
+{
+    for (int i = 0; i < used; i++)
+        delete vect[i];
+    used = 0;
+}
+
+template <class T> void Svec<T>::sort(int (*compare)(const T *a, const T *b))
+{
+    qsort(vect, used, sizeof(T), (compare_func)compare);
 }
 
 #endif // SVEC_H
