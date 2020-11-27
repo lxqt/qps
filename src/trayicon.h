4e4eff1ad68a5e4eb2530e7294883da6c840e801
tsujan <tsujan2000@gmail.com>
2020-11-27 16:13:32 +0330

    Fixed a crash when starting minimized to tray (#260)
    Fixes https://github.com/lxqt/qps/issues/258
    
    I also removed several redundant methods of the class `TrayIcon`. I don't know why the original author thought that, for example, `QSystemTrayIcon` had the method `mousePressEvent()` — maybe it was a `QWidget` with Qt2!

--- src/trayicon.h
+++ src/trayicon.h
@@ -37,65 +37,21 @@ class TrayIcon : public QSystemTrayIcon
 {
     Q_OBJECT
   public:
-    TrayIcon(const QPixmap &, const QString &, QMenu *popup = nullptr,
-             QWidget *parent = nullptr, const char *name = nullptr);
+    TrayIcon(const QPixmap &icon, const QString &tooltip, QMenu *popup = nullptr,
+             QWidget *parent = nullptr);
     ~TrayIcon() override;
 
-    // use WindowMaker dock mode.  ignored on non-X11 platforms
-    void setWMDock(bool use) { isWMDock = use; }
-    bool checkWMDock() { return isWMDock; }
     bool hasSysTray;
-    void setSysTray(bool /*val*/)
-    { // boolSysTray=val;
-    }
-
-    // Set a popup menu to handle RMB
-    void setPopup(QMenu *);
-    QMenu *popup() const;
 
     QPixmap icon() const;
 
     void sysInstall();
-    void sysRemove();
-
-    void init_TrayIconFreeDesktop();
-    void init_WindowMakerDock();
 
   public slots:
     void setIcon(const QPixmap &icon);
-    void setToolTip(const QString &tip);
-signals:
-    void clicked(const QPoint &);
-    void clicked(const QPoint &, int);
-    void doubleClicked(const QPoint &);
-    void closed();
-
-  protected:
-    //	bool event( QEvent *e );
-    virtual void mouseMoveEvent(QMouseEvent *e);
-    virtual void mousePressEvent(QMouseEvent *e);
-    virtual void mouseReleaseEvent(QMouseEvent *e);
-    virtual void mouseDoubleClickEvent(QMouseEvent *e);
-    virtual void leaveEvent(QEvent *);
-
-    void paintEvent(QPaintEvent *);
-    void resizeEvent(QResizeEvent *e);
-    //	void hideEvent ( QHideEvent *e ); // called after hide()
-    void closeEvent(QCloseEvent *e);
 
   private:
-    QMenu *pop;
     QPixmap pm;
-    QString tip;
-    QPoint tip_pos;
-
-    bool isWMDock;
-    //	bool boolSysTray; //DEL?
-    bool inTray;
-    bool flag_systray_ready;
-    bool flag_show_tip;
-
-    // DEL void sysUpdateToolTip();
 };
 
 #endif // CS_TRAYICON_H
