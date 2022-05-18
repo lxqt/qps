64afc7cc88dce9055530d35cdafa494f14fac88c
Weblate <noreply@weblate.org>
2022-05-18 03:09:33 +0200

    Weblate commits
    Co-authored-by: Taras Panchenko <top39@ukr.net>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qps/ru/
    Translation: LXQt Desktop/qps

--- src/translations/qps_ru.ts
+++ src/translations/qps_ru.ts
@@ -1353,7 +1353,7 @@ exited with status </source>
     <message>
         <location filename="../qps.cpp" line="470"/>
         <source>Hangup</source>
-        <translation>Отправить SIGHUP</translation>
+        <translation>Разорвать</translation>
     </message>
     <message>
         <location filename="../qps.cpp" line="471"/>
