c8a8e4b20776e40cdcbb46260d10a66b0964cdfd
Weblate <noreply@weblate.org>
2022-07-11 12:09:53 +0200

    Weblate commits
    Co-authored-by: Baptiste H <baptiste.huchon@bechamail.fr>
    Co-authored-by: Dr. Ayhan YALÇINSOY <ayhanyalcinsoy@pisilinux.org>
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Co-authored-by: Standreas <standreas@riseup.net>
    Co-authored-by: cybercop <cybercop_montana@abv.bg>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qps/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qps/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qps/it/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qps/nl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qps/tr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qps/uk/
    Translation: LXQt Desktop/qps

--- src/translations/qps_bg.ts
+++ src/translations/qps_bg.ts
@@ -1374,7 +1374,7 @@ exited with status </source>
     <message>
         <location filename="../qps.cpp" line="477"/>
         <source>Other Signals</source>
-        <translation type="unfinished"></translation>
+        <translation>Други сигнали</translation>
     </message>
     <message>
         <location filename="../qps.cpp" line="478"/>
