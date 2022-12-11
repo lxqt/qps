46dfa7db648317ee87eb6e0de4c46030ab12983f
LXQtBot <72348767+LXQtBot@users.noreply.github.com>
2022-12-11 16:43:50 +0800

    Weblate commits (#412)
    Co-authored-by: Ali Alhaidary علي الحيدري <ali.alhaidary@the5stars.org>
    Co-authored-by: Baptiste Huchon <baptiste.huchon@ikmail.com>
    Co-authored-by: Dimitrios Glentadakis <dglent@free.fr>
    Co-authored-by: Heimen Stoffels <vistausss@fastmail.com>
    Co-authored-by: Ihor Hordiichuk <igor_ck@outlook.com>
    Co-authored-by: Standreas <standreas@riseup.net>
    Co-authored-by: cybercop <cybercop_montana@abv.bg>
    Co-authored-by: 이정희 <daemul72@gmail.com>
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qps/ar/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qps/bg/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qps/el/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qps/fr/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qps/it/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qps/ko/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qps/nl/
    Translate-URL: https://translate.lxqt-project.org/projects/lxqt-desktop/qps/uk/
    Translation: LXQt Desktop/qps

--- src/translations/qps_bg.ts
+++ src/translations/qps_bg.ts
@@ -245,12 +245,12 @@
     <message>
         <location filename="../infobar.cpp" line="782"/>
         <source>I/O Summary:</source>
-        <translation type="unfinished"></translation>
+        <translation>I/O Резюме:</translation>
     </message>
     <message>
         <location filename="../infobar.cpp" line="816"/>
         <source>No input or output</source>
-        <translation type="unfinished"></translation>
+        <translation>Няма вход или изход</translation>
     </message>
 </context>
 <context>
@@ -959,7 +959,7 @@
         <location filename="../infobar.cpp" line="430"/>
         <location filename="../infobar.cpp" line="437"/>
         <source>User: </source>
-        <translation type="unfinished"></translation>
+        <translation>Потребител: </translation>
     </message>
     <message>
         <location filename="../infobar.cpp" line="430"/>
@@ -967,31 +967,31 @@
         <location filename="../infobar.cpp" line="437"/>
         <location filename="../infobar.cpp" line="438"/>
         <source>%, </source>
-        <translation type="unfinished"></translation>
+        <translation>%, </translation>
     </message>
     <message>
         <location filename="../infobar.cpp" line="431"/>
         <location filename="../infobar.cpp" line="438"/>
         <source>System: </source>
-        <translation type="unfinished"></translation>
+        <translation>Система: </translation>
     </message>
     <message>
         <location filename="../infobar.cpp" line="432"/>
         <location filename="../infobar.cpp" line="439"/>
         <source>Nice: </source>
-        <translation type="unfinished"></translation>
+        <translation>Добро: </translation>
     </message>
     <message>
         <location filename="../infobar.cpp" line="432"/>
         <location filename="../infobar.cpp" line="439"/>
         <source>%</source>
-        <translation type="unfinished">%</translation>
+        <translation>%</translation>
     </message>
     <message>
         <location filename="../infobar.cpp" line="471"/>
         <location filename="../infobar.cpp" line="506"/>
         <source>Total: </source>
-        <translation type="unfinished"></translation>
+        <translation>Общо: </translation>
     </message>
     <message>
         <location filename="../infobar.cpp" line="474"/>
@@ -1000,43 +1000,43 @@
         <location filename="../infobar.cpp" line="509"/>
         <location filename="../infobar.cpp" line="512"/>
         <source>, </source>
-        <translation type="unfinished"></translation>
+        <translation>, </translation>
     </message>
     <message>
         <location filename="../infobar.cpp" line="474"/>
         <location filename="../infobar.cpp" line="512"/>
         <source>Used: </source>
-        <translation type="unfinished"></translation>
+        <translation>Използвано: </translation>
     </message>
     <message>
         <location filename="../infobar.cpp" line="478"/>
         <source>Cached: </source>
-        <translation type="unfinished"></translation>
+        <translation>Кеширано: </translation>
     </message>
     <message>
         <location filename="../infobar.cpp" line="481"/>
         <source>Buffer: </source>
-        <translation type="unfinished"></translation>
+        <translation>Буфер: </translation>
     </message>
     <message>
         <location filename="../infobar.cpp" line="509"/>
         <source>Free: </source>
-        <translation type="unfinished"></translation>
+        <translation>Свободно: </translation>
     </message>
     <message>
         <location filename="../infobar.cpp" line="551"/>
         <source>Time passed after system boot</source>
-        <translation type="unfinished"></translation>
+        <translation>Изминало време след зареждане на системата</translation>
     </message>
     <message>
         <location filename="../infobar.cpp" line="579"/>
         <source>Average CPU load each 1, 5 ,15 minutes</source>
-        <translation type="unfinished"></translation>
+        <translation>Средно натоварване на процесора на всеки 1, 5, 15 минути</translation>
     </message>
     <message>
         <location filename="../infobar.cpp" line="724"/>
         <source>CPU Usage Summary:</source>
-        <translation type="unfinished"></translation>
+        <translation>Обобщение на използването на процесора:</translation>
     </message>
 </context>
 <context>
@@ -1059,7 +1059,7 @@
     <message>
         <location filename="../qps.cpp" line="179"/>
         <source>Session</source>
-        <translation type="unfinished"></translation>
+        <translation>Сесия</translation>
     </message>
     <message>
         <location filename="../qps.cpp" line="183"/>
